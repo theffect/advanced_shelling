11edb2cb073d558aad8596612b33a30b84e8a273
Shimon Zadok <shimonz@startpo.co.il>
2015-01-23 17:38:50 +0200

    one mode, no recursion

--- load.sh
+++ load.sh
@@ -1,10 +1,55 @@
 
-DEV_SCRIPTS="dev_scripts"
-DEV_BASE_ITEM=".git"
+
+BASE_ITEM=.base
 
 BASE_PATH=$PWD
 BACK_BASE=${PWD%/*}
-DEV_PATH=$BASE_PATH/$DEV_SCRIPTS
+
+add_paths() {
+	for CUR_PATH in "${MODE_PATH[@]}"; do
+		local ADD_PATH=$BASE_PATH/$CUR_PATH
+		add_path "$ADD_PATH"
+	done
+}
+
+add_path() {	
+	local ADD_PATH=$1
+	[[ ! $PATH =~ $ADD_PATH ]] && echo Adding $ADD_PATH to PATH && export PATH=$ADD_PATH:$PATH
+}
+
+mode_chk() {
+	
+	# Already in a mode
+	[ ! -z $CURRENT_SHELL_MODE ] && return
+	
+	# No new mode is required
+        [ ! -e "./$BASE_ITEM" ] && return
+	
+	# Load mode variables
+	source $BASE_ITEM
+	
+	if [ $CURRENT_SHELL_MODE != $SHELL_MODE ]; then
+		export CURRENT_SHELL_MODE=$SHELL_MODE
+		export OLD_PS1=$PS1
+		
+		add_paths 
+		export PS1='\u@\h:$(path_base)-\e[1;34m$(git_repo_name)\e[m\$ '
+	else
+		export PS1=$OLD_PS1
+	fi
+}
+
+find_base() {
+        local CUR_BASE=$PWD
+        local CONT=1
+
+        while [[ ! -z "$CUR_BASE" && ! -e $CUR_BASE/$DEV_BASE_ITEM ]]; do
+                local CUR_BASE=${PWD%/*}
+                cd ..
+        done
+
+        [ -z "$CUR_BASE" ] && return 0
+}
 
 path_base() {
 	local CURR=${PWD#$BASE_PATH}
@@ -15,22 +60,4 @@ path_base() {
 	fi
 }
 
-repo_name() {
-	local CUR_BASE=$PWD
-	local CONT=1
-	
-	while [[ ! -z "$CUR_BASE" && ! -e $CUR_BASE/$DEV_BASE_ITEM ]]; do
-		local CUR_BASE=${PWD%/*}
-		cd ..
-	done
-	
-	[ -z "$CUR_BASE" ] && return 0
-	
-	local TMP=$(grep url $CUR_BASE/$DEV_BASE_ITEM/config)
-	echo ${TMP##*/}
-	#${CUR_BASE##*/}
-}
-
-[[ ! $PATH =~ $DEV_SCRIPTS ]] && echo Adding $DEV_PATH to PATH && export PATH=$DEV_PATH:$PATH
-
-export PS1='\u@\h:$(path_base)-\e[1;34m$(repo_name)\e[m\$ '
+export PROMPT_COMMAND="mode_chk"
