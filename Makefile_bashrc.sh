a18e3618249dbc88df913f9ef6b1d760a51a411f
Shimon Zadok <shimonz@startpo.co.il>
2019-09-14 15:53:04 +0300

    rename loading function to 'as-load'

--- Makefile_bashrc.sh
+++ Makefile_bashrc.sh
@@ -8,11 +8,11 @@ case "$action" in
 		echo "Please add this to the .bashrc file in your home directory."
 		cat << EOF
 if [ -f /usr/sbin/advanced_shelling/load.sh ]; then
-   as_load() {
+   as-load() {
       echo "Using advanced shelling"
       source /usr/sbin/advanced_shelling/load.sh
    }
-   as_load
+   as-load
 fi
 EOF
 	;;
